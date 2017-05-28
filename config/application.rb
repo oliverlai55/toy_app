ece0cc895aa66f88f6e4b6b8afbc3f3ed139ef74
oliverlai55 <oliverlai55@gmail.com>
2017-05-28 19:19:20 -0400

    Fix code

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require_relative 'boot'
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -8,8 +7,6 @@ Bundler.require(*Rails.groups)
 
 module ToyApp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
